5a3e8ce03ff31cdd28980acce94f6cafd1fbbadc
Fangwei Lin <fangweilin@Fangweis-MacBook-Pro.local>
2019-08-18 16:50:31 -0700

    A Location-based Android App for Sharing Updates

---
+++ app/src/main/java/com/example/eventreporter/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.eventreporter;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+    }
+
+}
